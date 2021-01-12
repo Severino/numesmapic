7ee777f33501d13b2b812bf7210340c757ad9fc0
Severin Opel <severin.opel@outlook.com>
2021-01-12 16:31:55 +0100

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Map />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Map from "./components/Map.vue";
+
+@Component({
+  components: {
+    Map
+  }
+})
+export default class App extends Vue {}
+</script>
+
+
+<style>
+
+body, html {
+  margin: 0;
+  }
+
+    #app {
+      display: flex;
+      flex-direction: column;
+      height: 100vh;
+      background-color: gray;
+      }
+
+</style>
